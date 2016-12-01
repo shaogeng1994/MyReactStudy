3b5661b62e78377b5cabf304e568d59c7c88f807
shao <shaogeng1994@outlook.com>
2016-12-01 17:59:37 +0800

    first

--- demo3/test.js
+++ demo3/test.js
@@ -1,4 +1,53 @@
+class Clock extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {date : new Date(),day : 'dark'};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            ()=> this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({date : new Date(),day : 'dark'});
+    }
+
+    render(props) {
+       return (
+            <div>
+                <h1>Hello,world！</h1>
+                <h1>It is {this.state.date.toLocaleTimeString()}.</h1>
+                <h1>It is {this.state.day}.</h1>
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(
-    <h1>Hello,world！</h1>,
+    <Clock />,
     document.getElementById('root')
-);
+)
+
+function tick() {
+    ReactDOM.render(
+            <Clock date={new Date()}/>,
+            document.getElementById('root')
+        );
+}
+
+
+
+
+
+
+// ReactDOM.render(
+//     <Clock date = {new Date()}/>,
+//     document.getElementById('root')
+// );
